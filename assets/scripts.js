3573601f6c7a0b793862492770d920d9b5318c24
JulienSuan <julienlemolo@gmail.com>
2023-06-16 12:54:26 +0200

    optimisations des images à fond

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,17 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+(() => {
+    const baseUrl = 'https://res.cloudinary.com/di897duch/image/upload'
+    console.log(baseUrl)
+    Array.from(document.querySelectorAll('img')).forEach(image => {
+        const { clientWidth, clientHeight } = image
+        console.dir(image)
+      const pixelRatio = window.devicePixelRatio || 1.0
+      const imageParams = `w_${100 * Math.round(clientWidth * pixelRatio / 100)},h_${100 * Math.round(clientHeight * pixelRatio / 100)},c_fill,g_auto,f_auto`
+      const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`
+      image.src = url
+    })
+  })()
+  

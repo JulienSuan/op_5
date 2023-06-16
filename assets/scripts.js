f6ddb40ebba868a2b98c23dae58f94e9a9c35044
JulienSuan <julienlemolo@gmail.com>
2023-06-16 19:20:30 +0200

    gros changement images pour opti v2

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,25 +14,27 @@ $(document).ready(function() {
     });
 });
 
-
 window.addEventListener('load', () => {
+    
     const baseUrl = 'https://res.cloudinary.com/di897duch/image/upload'
-    console.log(baseUrl)
+    const activeImage = document.querySelector('.carousel-item.active img');
+    const width = activeImage.offsetWidth;
+    const height = activeImage.offsetHeight;
+    const carouselImages = document.querySelectorAll('.carousel-item:not(.active) img');
     Array.from(document.querySelectorAll('img')).forEach(image => {
         if (image.alt == "Logo instagram") {
             return
         }
-        const { offsetWidth, offsetHeight} = image
-        if (offsetWidth && offsetHeight == 0) {
-         const activeImage = document.querySelector('.carousel-item.active img');
-        const { offsetWidth, offsetHeight} = activeImage
-        const pixelRatio = window.devicePixelRatio || 1.0
-        const imageParams = `w_${100 * Math.round(offsetWidth * pixelRatio / 100)},h_${100 * Math.round(offsetHeight * pixelRatio / 100)},c_fill,g_auto,f_auto`
-        const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`
-        image.src = url
+        if (image.offsetHeight == 0 && image.offsetHeight == 0) {
+            const { offsetWidth, offsetHeight} = activeImage
+            console.log(offsetWidth, offsetHeight)
+            const pixelRatio = window.devicePixelRatio || 1.0
+            const imageParams = `w_${100 * Math.round(offsetWidth * pixelRatio / 100)},h_${100 * Math.round(offsetHeight * pixelRatio / 100)},c_fill,g_auto,f_auto`
+            const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`
+            image.src = url
         } else {
-        const { offsetWidth, offsetHeight} = activeImage
-
+            const { offsetWidth, offsetHeight} = image
+            console.log(offsetWidth, offsetHeight)
             const pixelRatio = window.devicePixelRatio || 1.0
             const imageParams = `w_${100 * Math.round(offsetWidth * pixelRatio / 100)},h_${100 * Math.round(offsetHeight * pixelRatio / 100)},c_fill,g_auto,f_auto`
             const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`
